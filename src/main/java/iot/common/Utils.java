0828eb5438e1fb29eb306f5a16fc5e290a6ed583
Clever Cavy <clever.cavy@gmail.com>
2016-06-26 21:58:02 +0300

    first debug screen

---
+++ src/main/java/iot/common/Utils.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015 - 2016
+ *
+ * @author Clever Cavy / clever.cavy@gmail.com
+ */
+package iot.common;
+
+/**
+ * @author Dejavu
+ * @version 0.1
+ * @since 0.1
+ */
+public class Utils {
+}
---
+++ src/main/java/iot/face/FaceController.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015 - 2016
+ *
+ * @author Clever Cavy / clever.cavy@gmail.com
+ */
+package iot.face;
+
+/**
+ * @author Dejavu
+ * @version 0.1
+ * @since 0.1
+ */
+public class FaceController {
+}
